199f07cc8c4756889bb98aee09b5d4d68006ffc2
Ruben <RubenGomes19@users.noreply.github.com>
2020-07-09 23:21:54 +0100

    Teste comando por voz.

---
+++ app/src/main/java/pt/ipg/rccar/ModoVoz.java
@@ -0,0 +1,14 @@
+package pt.ipg.rccar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModoVoz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modo_voz);
+    }
+}
