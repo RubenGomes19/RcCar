245f0398082600e3d1867d7035beb6999fdb9253
Ruben <RubenGomes19@users.noreply.github.com>
2020-07-09 23:01:12 +0100

    Teste comando por voz.

--- app/src/main/java/pt/ipg/rccar/ModoAutonomo.java
+++ app/src/main/java/pt/ipg/rccar/ModoAutonomo.java
@@ -3,25 +3,33 @@ package pt.ipg.rccar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothSocket;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class ModoAutonomo extends AppCompatActivity {
     Button btnLed2, buttonPress;
+    TextView textViewTexto;
     private int colorFlag = 0;
     private boolean clicando = true;
+
+    private static final int ID_TEXTO_PARA_VOZ = 100;
 
 
 
@@ -41,6 +49,7 @@ public class ModoAutonomo extends AppCompatActivity {
 
         btnLed2 = (Button) findViewById(R.id.btnLed2);
         buttonPress = (Button) findViewById(R.id.buttonPress);
+        textViewTexto = (TextView) findViewById(R.id.textViewTexto);
 
 
         btnLed2.setOnClickListener(new View.OnClickListener() {
@@ -68,7 +77,33 @@ public class ModoAutonomo extends AppCompatActivity {
 
         });
 
-        buttonPress.setOnTouchListener(new View.OnTouchListener() {
+
+       buttonPress.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent iVoz = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+               iVoz.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+               iVoz.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+               iVoz.putExtra(RecognizerIntent.EXTRA_PROMPT, "Fale algo");
+
+               try{
+                   startActivityForResult(iVoz, ID_TEXTO_PARA_VOZ);
+               }catch (ActivityNotFoundException a){
+                   Toast.makeText(getApplicationContext(), "Este Dispositivo não suporta comando por voz!", Toast.LENGTH_LONG).show();
+               }
+            }
+       });
+
+
+
+
+
+
+
+        /*buttonPress.setOnTouchListener(new View.OnTouchListener() {
 
 
             @Override
@@ -100,6 +135,8 @@ public class ModoAutonomo extends AppCompatActivity {
             }
         });
 
+         */
+
 /*
         buttonPress.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
@@ -127,5 +164,34 @@ public class ModoAutonomo extends AppCompatActivity {
 */
 
     }
+
+    @Override
+    protected void onActivityResult(int id, int resultCodeID, Intent dados) {
+        super.onActivityResult(id, resultCodeID, dados);
+
+        switch (id){
+            case ID_TEXTO_PARA_VOZ:
+                if(resultCodeID == RESULT_OK && null != dados){
+                    ArrayList<String> result = dados.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    String texto = result.get(0);
+
+                    Toast.makeText(getApplicationContext(), "Fala: " + texto, Toast.LENGTH_LONG).show();
+
+                    textViewTexto.setText(texto);
+
+                    String start = "start";
+
+                    if(texto.equals(start) ){
+                        MainActivity.connectedThread.enviar("l");
+                        Toast.makeText(getApplicationContext(), "Sao iguais: " + texto + " = " + start, Toast.LENGTH_LONG).show();
+                    }
+
+                }
+                break;
+        }
+
+    }
+
 
 }
